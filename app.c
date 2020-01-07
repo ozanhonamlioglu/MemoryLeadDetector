678ebc0ea7b7ed6bd453e7fd575b282646d70e36
ozan <ol1915101012@isparta.edu.tr>
2020-01-07 17:59:17 +0300

    Initial commit

---
+++ app.c
@@ -0,0 +1,84 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  app.c
+ *
+ *    Description:  This file represents the application demonstrating the use of MLD library
+ *
+ *        Version:  1.0
+ *        Created:  Thursday 28 February 2019 06:41:32  IST
+ *       Revision:  1.0
+ *       Compiler:  gcc
+ *
+ *         Author:  Er. Abhishek Sagar, Networking Developer (AS), sachinites@gmail.com
+ *        Company:  Brocade Communications(Jul 2012- Mar 2016), Current : Juniper Networks(Apr 2017 - Present)
+ *        
+ *        This file is part of the MLD distribution (https://github.com/sachinites).
+ *        Copyright (c) 2017 Abhishek Sagar.
+ *        This program is free software: you can redistribute it and/or modify
+ *        it under the terms of the GNU General Public License as published by  
+ *        the Free Software Foundation, version 3.
+ *
+ *        This program is distributed in the hope that it will be useful, but 
+ *        WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ *        General Public License for more details.
+ *
+ *        You should have received a copy of the GNU General Public License 
+ *        along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * =====================================================================================
+ */
+
+#include "mld.h"
+#include <memory.h>
+#include <stdlib.h>
+
+/*Application Structures*/
+
+typedef struct emp_ {
+
+    char emp_name[30];
+    unsigned int emp_id;
+    unsigned int age;
+    struct emp_ *mgr;
+    float salary;
+} emp_t;
+
+typedef struct student_{
+
+    char stud_name[32];
+    unsigned int rollno;
+    unsigned int age;
+    float aggregate;
+    struct student_ *best_colleage;
+} student_t;
+
+int
+main(int argc, char **argv){
+
+    /*Step 1 : Initialize a new structure database */
+    struct_db_t *struct_db = calloc(1, sizeof(struct_db_t));
+    
+    /*Create structure record for structure emp_t*/
+    static field_info_t emp_fields[] = {
+        FIELD_INFO(emp_t, emp_name, CHAR,    0),
+        FIELD_INFO(emp_t, emp_id,   UINT32,  0),
+        FIELD_INFO(emp_t, age,      UINT32,  0),
+        FIELD_INFO(emp_t, mgr,      OBJ_PTR, emp_t),
+        FIELD_INFO(emp_t, salary,   FLOAT, 0)
+    };
+    REG_STRUCT(struct_db, emp_t, emp_fields);
+
+    static field_info_t stud_fiels[] = {
+        FIELD_INFO(student_t, stud_name, CHAR, 0),
+        FIELD_INFO(student_t, rollno,    UINT32, 0),
+        FIELD_INFO(student_t, age,       UINT32, 0),
+        FIELD_INFO(student_t, aggregate, FLOAT, 0),
+        FIELD_INFO(student_t, best_colleage, OBJ_PTR, student_t)
+    };
+    REG_STRUCT(struct_db, student_t, stud_fiels);
+
+    print_structure_db(struct_db);
+    return 0;
+}
